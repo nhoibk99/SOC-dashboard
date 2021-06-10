67ec984c3f945e96a855a9169028f46df431fab6
liemnt <liemnt@vninfosec.net>
2021-06-10 17:50:21 +0700

    temp dialog

--- src/views/topoGraph/topoGraph.jsx
+++ src/views/topoGraph/topoGraph.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import Graph from "react-graph-vis";
 import './topoGraph.styles.scss';
 import {AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem  } from '@material-ui/core';
+import {makeStyles, 
+     Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
+     FormControl, FormControlLabel, InputLabel, Select, Switch} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles((theme) => ({
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: 'auto',
+      width: 'fit-content',
+    },
+    formControl: {
+      marginTop: theme.spacing(2),
+      minWidth: 120,
+    },
+    formControlLabel: {
+      marginTop: theme.spacing(1),
+    },
+  }));
 class TopoGraph extends React.Component{
     constructor(props) {
         super(props);
@@ -38,6 +56,9 @@ class TopoGraph extends React.Component{
             auth: true,
             anchorEl: null,
             open: false,
+            openDialog: false,
+            fullWidth: true,
+            maxWidth: 'sm',
         }
     }
     
@@ -112,6 +133,29 @@ class TopoGraph extends React.Component{
             open: false,
         })
     }
+    handleClickOpen = () => {
+        this.setState({
+            openDialog: true,
+        })
+    }
+    
+    handleClose = () => {
+        this.setState({
+            openDialog: false,
+        })
+    }
+    
+    handleMaxWidthChange = (event) => {
+        this.setState({
+            maxWidth: event.target.value,
+        })
+    }
+    
+    handleFullWidthChange = (event) => {
+        this.setState({
+            fullWidth: event.target.checked,
+        })
+    }
 
     render() {
     
@@ -142,6 +186,7 @@ class TopoGraph extends React.Component{
             }
             
         };
+        // const classes = useStyles();
         
         return (
             <div>
@@ -166,7 +211,8 @@ class TopoGraph extends React.Component{
                                 open={this.state.open}
                                 onClose={this.handleClose}
                             >
-                                <MenuItem onClick={this.createNode}>Create node</MenuItem>
+                                {/* <MenuItem onClick={this.createNode}>Create node</MenuItem> */}
+                                <MenuItem onClick={this.handleClickOpen}>Create node</MenuItem>
                                 <MenuItem onClick={this.deleteNode}>Delete node</MenuItem>
                                 <MenuItem onClick={this.handleClose}>Change edge</MenuItem>
                             </Menu>
@@ -176,19 +222,59 @@ class TopoGraph extends React.Component{
                     </Toolbar>
                 </AppBar>
                 <div className='topoGraph'>
-                    {/* <p style={{color: 'yellow'}}>currentNode:{this.state.currentNode}</p>
-                    <button className='addNode' onClick={this.createNode}> Add node</button>
-                    <button className='deleteNode' onClick={this.deleteNode}> Delete node</button> */}
                     <Graph
                         graph={this.state.graph}
                         options={options}
                         events={events}
-                        // getNetwork={network => {
-                        // //  if you want access to vis.js network api you can set the state in a parent component using this property
-                        // }}
-                        
                     />
                 </div>
+                <Dialog
+                    fullWidth={this.state.fullWidth}
+                    maxWidth={this.state.maxWidth}
+                    open={this.state.openDialog}
+                    onClose={this.handleClose}
+                    aria-labelledby="max-width-dialog-title"
+                >
+                    <DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText>
+                        You can set my maximum width and whether to adapt or not.
+                    </DialogContentText>
+                    {/* <form className={classes.form} noValidate>
+                        <FormControl className={classes.formControl}> */}
+                    <form  noValidate>
+                        <FormControl>
+                        <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+                        <Select
+                            autoFocus
+                            value={this.state.maxWidth}
+                            onChange={this.handleMaxWidthChange}
+                            inputProps={{
+                            name: 'max-width',
+                            id: 'max-width',
+                            }}
+                        >
+                            <MenuItem value={false}>false</MenuItem>
+                            <MenuItem value="xs">xs</MenuItem>
+                            <MenuItem value="sm">sm</MenuItem>
+                            <MenuItem value="md">md</MenuItem>
+                            <MenuItem value="lg">lg</MenuItem>
+                            <MenuItem value="xl">xl</MenuItem>
+                        </Select>
+                        </FormControl>
+                        <FormControlLabel
+                        // className={classes.formControlLabel}
+                        control={<Switch checked={this.state.fullWidth} onChange={this.handleFullWidthChange} />}
+                        label="Full width"
+                        />
+                    </form>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Close
+                    </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
